dc11117450531d140b5273a9b2f4cfef0804a890
Harsh Morey <68352095+HarshMorey@users.noreply.github.com>
2021-04-29 23:23:27 +0530

    Updates

---
+++ precache-manifest.7eb573298e90939c4094729cbba97114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bc4095df4e4ae4585cd407750fe0b7",
+    "url": "/MyTask/index.html"
+  },
+  {
+    "revision": "ae551f35ab3c393197e8",
+    "url": "/MyTask/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "4ca1b05d95b66f60a79c",
+    "url": "/MyTask/static/css/main.92a51d7f.chunk.css"
+  },
+  {
+    "revision": "ae551f35ab3c393197e8",
+    "url": "/MyTask/static/js/2.eb9c30f9.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/MyTask/static/js/2.eb9c30f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca1b05d95b66f60a79c",
+    "url": "/MyTask/static/js/main.e6982333.chunk.js"
+  },
+  {
+    "revision": "dcbb215c11e0878a12bc",
+    "url": "/MyTask/static/js/runtime-main.4dd6a837.js"
+  },
+  {
+    "revision": "68bfe60c7edb6ddf8cf5002d830d184a",
+    "url": "/MyTask/static/media/song.68bfe60c.mp3"
+  }
+]);
